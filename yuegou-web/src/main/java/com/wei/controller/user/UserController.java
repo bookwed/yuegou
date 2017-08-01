a081595a21c5f34ae74e89437b987306528a8d84
weierdong <weierdong@yupiao.com>
2017-08-01 11:55:54 +0800

    modify on dev

--- yuegou-web/src/main/java/com/wei/controller/user/UserController.java
+++ yuegou-web/src/main/java/com/wei/controller/user/UserController.java
@@ -31,13 +31,7 @@ public class UserController {
     }
 
     public static void main(String[] args) {
-
-        int sum = 0;
-        for (int i = 0;i<=100;i++){
-
-        }
-
-
+        System.out.println("dev");
     }
 
 
